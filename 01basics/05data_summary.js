e242263a3d1e51deb41dbead2a34a9735e884954
Sarvesh Kr. Yadav <81284206+sarveshyadavcs@users.noreply.github.com>
2023-09-16 15:03:46 +0000

    Add data type work

--- 01basics/05data_summary.js
+++ 01basics/05data_summary.js
@@ -4,41 +4,64 @@
 
 
 /* Primtive */
-const id1 = Symbol("abc");
-const id2 = Symbol("abc");
-console.log(id1);
-console.log(id2);
-console.log(id1 === id2); //false
+// const id1 = Symbol("abc");
+// const id2 = Symbol("abc");
+// console.log(id1);
+// console.log(id2);
+// console.log(id1 === id2); //false
 
-let id = Symbol("id");
-let user = {
-  name: "John",
-  id: 123, // not "id": 123 //id: 123
-  [id]: 123, // not "id": 123  //[Symbol(id)]: 123
-};
-console.log(user);
+// let id = Symbol("id");
+// let user = {
+//   name: "John",
+//   id: 123, // not "id": 123 //id: 123
+//   [id]: 123, // not "id": 123  //[Symbol(id)]: 123
+// };
+// console.log(user);
 
-const bigNum = 312312431254345232345n; //bigInt
-console.log(bigNum);
-console.log(typeof bigNum);
+// const bigNum = 312312431254345232345n; //bigInt
+// console.log(bigNum);
+// console.log(typeof bigNum);
 
 /* Non Primtive */
 //Array 
-const hero = ["ram", "shyam", "narayan"];
-console.log(hero);
+// const hero = ["ram", "shyam", "narayan"];
+// console.log(hero);
 
-//Object
-let myObj = {name: "Rma", age: 22};
-console.log(myObj)
+// //Object
+// let myObj = {name: "Rma", age: 22};
+// console.log(myObj)
 
 //function
-const myfunction = function(){
-    return console.log("Hi...")
-}
-myfunction()
+// const myfunction = function(){
+//     return console.log("Hi...")
+// }
+// myfunction()
 
-console.log(typeof myfunction);//function
+// console.log(typeof myfunction);//function
 
 
 //Official doc url for study purpose
 //https://262.ecma-international.org/13.0/#sec-typeof-operator
+
+//Memory for datatype
+//Primtive for use Stack: copy value
+//Non-Primtive for use Heap: reference value
+
+let myName = "SarvUttam";
+let newName = myName; //Copy original value here So that do not change in original value
+newName = "SarvGun"
+console.log(myName);
+console.log(newName); 
+
+let myStu = ["ram", "kumar", "yadav"];
+let newStu = myStu; //reference here So that will change in original value
+newStu[0] = "shyam";
+console.log(myStu);
+console.log(newStu);
+
+
+let obj = {name:"Krishna", age:22};
+let newObj = obj;
+newObj.name = "Kanhai";
+console.log(obj);
+console.log(newObj);
