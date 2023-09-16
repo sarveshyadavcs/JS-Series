0140a69caa94004dd8a128325f5110409876fa0f
Sarvesh Kr. Yadav <81284206+sarveshyadavcs@users.noreply.github.com>
2023-09-16 11:27:00 +0000

    variable

---
+++ 01basics/02datatype.js
@@ -0,0 +1,21 @@
+"use strict"; // teart all js code as a newer version
+
+//alert("hi" + 3 + 3)// we are using note js not browser so not showing alert
+console.log(3+2);
+//Code readability should be heigh
+
+
+//*******Datatypes
+//number => 2 to power 53
+//bigint =>
+//string => ""
+//boolean => true, false
+//null => standalone value / this is a represetative of the empaty value
+//undefined => Value is not defined/assign
+//symbol => make uniqueness of the variable 
+
+//object =>
+
+console.log(typeof "ram")
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
