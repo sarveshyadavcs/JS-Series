1a5b4e5229a99d00a6e3a8d604b29491335cbbed
Sarvesh Kr. Yadav <81284206+sarveshyadavcs@users.noreply.github.com>
2023-09-16 13:44:05 +0000

    compaison topic

---
+++ 01basics/04comparison.js
@@ -0,0 +1,32 @@
+// console.log(4>5); //false
+// console.log(4<5); //true
+// console.log(4>=5); //false
+// console.log(4<=5); //true
+// console.log(4==5); //false
+// console.log(4===5); //false
+// console.log(4!=5); //true
+// console.log(4!==5); //true
+
+// console.log(null>0) //false
+// console.log(null<0) //false
+// console.log(null>=0) //true
+// console.log(null<=0) //true
+// console.log(undefined>0) //false
+// console.log(undefined<0) //false
+// console.log(undefined>=0) //false
+// console.log(undefined<=0) //false
+
+// console.log("2">1) //true
+// console.log("02">1) //true
+// console.log("2"==2) //true
+// console.log("02"==2) //true
+// console.log("2"===2) //false
+// console.log("02"===2) //false
+
+
+// console.log(null==0) //false
+// console.log(undefined==0) //false
+//console.log(null==undefined) //true
+console.log(null===undefined) //false
+console.log(undefined===0) //false
+console.log(null===0) //false
