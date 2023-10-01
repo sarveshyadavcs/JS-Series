0a5a987e2c22013ff26f8a37d4e64eb56fb2f89b
Sarvesh Kr. Yadav <81284206+sarveshyadavcs@users.noreply.github.com>
2023-10-01 11:45:39 +0000

    for each completed

--- 05iteration/03higherOrder.js
+++ 05iteration/03higherOrder.js
@@ -43,3 +43,46 @@ console.log(map) //Map only consider a unique value on the basis of keys
 for (const [key, value] of map) {
     console.log(`${key} keyOf ${value}`)
 }
+
+//for In loop will not work for itration 
+// for(const key in map){
+//     console.log(key);
+// }
+
+map.forEach((val,key)=>{
+    // console.log(val)
+    // console.log(key)
+    console.log(val, key)
+})
+
+const myArray = ["dd", "cc", "ee"]
+myArray.forEach((val, key, newArray)=>{
+    // console.log(val)
+    // console.log(key)
+    // console.log(newArray[0])
+    console.log(val, key, newArray[0])
+})
+
+function print(item){console.log(item)};
+
+myArray.forEach(print) //o/p: dd cc ee
+
+
+const mySub = [
+    {
+        langName: "JS",
+        langFile: "js"
+    },
+    {
+        langName: "JAVA",
+        langFile: "java"
+    },
+    {
+        langName: "PYTHON",
+        langFile: "python"
+    }
+]
+
+mySub.forEach((item, key, arr)=>{
+    console.log(item.langName); //o/p: JS JAVA PYTHON
+})
